42f8aea76dea5f8869f43d6c99581230b0b9ca33
John Teske <john.teske@protonmail.com>
2020-11-10 20:28:44 -0800

    Use html5 <video>

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function (config) {
 
   config.addShortcode("embed", embed);
   config.addShortcode("videoEmbed", videoEmbed);
+
+  config.addShortcode("video", video);
 };
 
 function embed(url) {
@@ -11,4 +13,17 @@ function embed(url) {
 
 function videoEmbed(url) {
   return `<div class="video">${embed(url)}</div>`;
+}
+
+function attribution(author, title) {
+  return `<h3>${author}${title != null ? ": " + title : ""}</h3>`
+}
+
+function video(url, author, title) {
+  return `${attribution(author, title)}
+<div class="video">
+  <video src="${url}" controls>
+    <div class="fallback">Your browser doesn't support HTML5 video. You can <a href="${url}" target="_blank">view or download</a> this video directly.</div>
+  </video>
+</div>`
 }
