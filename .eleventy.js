42393c972c088dbfd9640b629bfdb4cd4d72b95d
John Teske <john.teske@protonmail.com>
2020-11-22 11:24:52 -0800

    Place favicon in assets

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ const video = require("./components/video");
 
 module.exports = function (config) {
   config.addPassthroughCopy("assets/*");
-  config.addPassthroughCopy("favicon.ico");
 
   config.addShortcode("audio", audio.custom);
   config.addShortcode("video", video.custom);
