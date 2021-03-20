e6b8233aaaf7d0a8ccb96aa575725bc1244fe146
John Teske <john.teske@protonmail.com>
2021-03-20 13:06:50 -0700

    Add alt attr to images

--- components/image.js
+++ components/image.js
@@ -3,6 +3,10 @@ const attribution = require("./attribution")
 module.exports = function(url, author, title) {
   return `${attribution(author, title)}
 <div class="constrain-aspect">
-  <img src="${url}" />
+  <img
+    data-src="${url}"
+    loading="lazy"
+    alt="'${title}' by ${author}"
+  />
 </div>`
 }
