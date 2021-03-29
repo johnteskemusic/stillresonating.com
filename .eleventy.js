de0c13f26560006f2e82c8eae8aef84ac4110ddd
John Teske <john.teske@protonmail.com>
2021-03-28 19:54:02 -0700

    Remove attribution shorttag

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,10 @@ const audio = require("./components/audio");
 const imageSubmission = require("./components/imageSubmission");
 const video = require("./components/video");
 const image = require("./config/image");
-const attribution = require("./components/attribution");
 
 module.exports = function (config) {
   config.addPassthroughCopy("assets/*");
 
-  config.addShortcode("attribution", attribution);
   config.addShortcode("audio", audio.custom);
   config.addNunjucksAsyncShortcode("imageSubmission", imageSubmission);
   config.addShortcode("video", video.custom);
