f78d37d312d4f9edd448a6daede592d65d87f6e5
John Teske <john.teske@protonmail.com>
2021-03-28 23:34:13 -0700

    Add htmlmin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const audio = require("./components/audio");
 const image = require("./components/image");
 const video = require("./components/video");
+const minify = require("./config/minify");
 
 module.exports = function (config) {
   config.addPassthroughCopy("assets/*");
@@ -9,12 +10,5 @@ module.exports = function (config) {
   config.addShortcode("image", image);
   config.addShortcode("video", video.custom);
 
-  // if feature.deferImages--although now this is a transform, is that needed?
-  // and if I consider this a plugin, how is it used in my main js script?
-  config.addTransform("deferImages", function(content, outputPath) {
-    if (!outputPath.endsWith("html")) {
-      return content
-    }
-    return content.replaceAll("img src", "img data-src");
-  })
+  config.addPlugin(minify);
 };
--- index.md
+++ index.md
@@ -1,12 +1,13 @@
 ---
 title: index
+description: a series of postcard works by composers John Teske, Neil Welch, and Nat Evans
 layout: layouts/default
 templateEngineOverride: njk,md
 ---
 
 <header class="constrain-aspect" id="splash">
 <div id="splash-card">
-  <img src="/assets/icon.png" height="48px" width="48px" style="align-self: flex-end"/>
+  <img src="/assets/icon.png" height="48px" width="48px" style="align-self: flex-end"/ alt="icon">
   <h1><em>still resonating</em></h1>
 
 {% include 'about.njk' %}
